9c14831f9fc0bea7f72b97af48f4f596cf98d7f1
Miguel Garzon <miguelzaz123@gmail.com>
2019-01-07 15:35:10 -0500

    New project

---
+++ cmds/60s.js
@@ -0,0 +1,9 @@
+
+module.exports.run = async (bot,message,args) => {
+    message.channel.send("Siguiente scrim en 1 minuto");
+}
+
+
+module.exports.help = {
+    name: "60s"
+}
---
+++ cmds/now.js
@@ -0,0 +1,10 @@
+
+module.exports.run = async (bot,message,args) => {
+
+    message.channel.send("Las scrims empiezan ahora!");
+}
+
+
+module.exports.help = {
+    name: "now"
+}
